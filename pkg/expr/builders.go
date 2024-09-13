1d092aca576716a8888d05c0beac2cbc68b92120
Andrei Gavrilov <benisgachi@gmail.com>
2024-09-13 18:03:12 +0300

    drop redundant interface

--- pkg/expr/builders.go
+++ pkg/expr/builders.go
@@ -2,59 +2,53 @@ package expr
 
 import "regexp"
 
-type SimpleExprBuilderSource interface {
-	FromPattern(pattern string) Expr
-	FromPatternAndExclude(pattern string, exclude string) Expr
-	FromRegex(regex *regexp.Regexp) Expr
-	FromRegexAndExclude(regex *regexp.Regexp, excludeRegex *regexp.Regexp) Expr
-}
-
-type simpleExprBuilder struct {
+type SimpleExprBuilder struct {
 	last  int
 	first int
 }
 
 // NewSimpleExpr returns builder that creates a simple expr that will process data as is
-func NewSimpleExpr() SimpleExprBuilderSource {
-	return &simpleExprBuilder{
+func NewSimpleExpr() *SimpleExprBuilder {
+	return &SimpleExprBuilder{
 		last:  0,
 		first: 0,
 	}
 }
 
-// NewSimpleExprLast returns builder that creates a simple expr that will process only last n bytes of data
-func NewSimpleExprLast(last int) SimpleExprBuilderSource {
-	return &simpleExprBuilder{
+// NewSimpleExprLast returns builder that creates a simple expr that will match only last n bytes of data
+func NewSimpleExprLast(last int) *SimpleExprBuilder {
+	return &SimpleExprBuilder{
 		last:  last,
 		first: 0,
 	}
 }
 
 // NewSimpleExprLast200 is a wrapper around NewSimpleExprLast with last fixed to 200
-func NewSimpleExprLast200() SimpleExprBuilderSource {
+func NewSimpleExprLast200() *SimpleExprBuilder {
 	return NewSimpleExprLast(200)
 }
 
 // NewSimpleExprLast20 is a wrapper around NewSimpleExprLast with last fixed to 20
-func NewSimpleExprLast20() SimpleExprBuilderSource {
+func NewSimpleExprLast20() *SimpleExprBuilder {
 	return NewSimpleExprLast(20)
 }
 
-func NewSimpleExprFirst(first int) SimpleExprBuilderSource {
-	return &simpleExprBuilder{
+// NewSimpleExprLast returns builder that creates a simple expr that will match only first n bytes of data
+func NewSimpleExprFirst(first int) *SimpleExprBuilder {
+	return &SimpleExprBuilder{
 		last:  0,
 		first: first,
 	}
 }
 
 // NewSimpleExprFirst200 is a wrapper around NewSimpleExprFirst with first fixed to 200
-func NewSimpleExprFirst200() SimpleExprBuilderSource {
+func NewSimpleExprFirst200() *SimpleExprBuilder {
 	return NewSimpleExprFirst(200)
 }
 
 // FromPattern creates a simple expr matcher from compiling given pattern.
 // Panics in case of compilation failure, use your own regex with FromRegex if passed pattern can be incorrect.
-func (s *simpleExprBuilder) FromPattern(pattern string) Expr {
+func (s *SimpleExprBuilder) FromPattern(pattern string) Expr {
 	return &simpleExpr{
 		exprs: []exprMatcher{
 			{
@@ -70,7 +64,7 @@ func (s *simpleExprBuilder) FromPattern(pattern string) Expr {
 // FromPatternAndExclude creates a simple expr matcher from compiling given match and exclude patterns.
 // It will only match data that is matched by regex and is not matched by exclude regex.
 // Panics in case of compilation failure, use your own regex with FromRegexAndExclude if passed patterns can be incorrect.
-func (s *simpleExprBuilder) FromPatternAndExclude(pattern string, exclude string) Expr {
+func (s *SimpleExprBuilder) FromPatternAndExclude(pattern string, exclude string) Expr {
 	return &simpleExpr{
 		exprs: []exprMatcher{
 			{
@@ -84,7 +78,7 @@ func (s *simpleExprBuilder) FromPatternAndExclude(pattern string, exclude string
 }
 
 // FromRegex creates a simple expr matcher from given regex
-func (s *simpleExprBuilder) FromRegex(regex *regexp.Regexp) Expr {
+func (s *SimpleExprBuilder) FromRegex(regex *regexp.Regexp) Expr {
 	return &simpleExpr{
 		exprs: []exprMatcher{
 			{
@@ -99,7 +93,7 @@ func (s *simpleExprBuilder) FromRegex(regex *regexp.Regexp) Expr {
 
 // FromRegexAndExclude creates a simple expr matcher from passed regex and exclude.
 // It will only match data that is matched by regex and is not matched by exclude regex.
-func (s *simpleExprBuilder) FromRegexAndExclude(regex *regexp.Regexp, excludeRegex *regexp.Regexp) Expr {
+func (s *SimpleExprBuilder) FromRegexAndExclude(regex *regexp.Regexp, excludeRegex *regexp.Regexp) Expr {
 	return &simpleExpr{
 		exprs: []exprMatcher{
 			{
