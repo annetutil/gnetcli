165761d77ffc297c90188c3ca576d866dde2f7d2
Gennady Lipenkov <lipenkov@gmail.com>
2024-12-08 17:01:12 +0300

    Fix Addr() for raw ipv6 address, add test cases

--- pkg/streamer/ssh/ssh_test.go
+++ pkg/streamer/ssh/ssh_test.go
@@ -14,3 +14,33 @@ func TestSSHInterface(t *testing.T) {
 	_, ok := interface{}(&val).(streamer.Connector)
 	assert.True(t, ok, "not a Connector interface")
 }
+
+func TestEndpoint_Addr(t *testing.T) {
+	tests := []struct {
+		name     string
+		endpoint Endpoint
+		expected string
+	}{
+		{
+			name:     "default",
+			endpoint: Endpoint{Host: "localhost", Port: 22},
+			expected: "localhost:22",
+		},
+		{
+			name:     "custom port",
+			endpoint: Endpoint{Host: "example.com", Port: 2222},
+			expected: "example.com:2222",
+		},
+		{
+			name:     "IPv6",
+			endpoint: Endpoint{Host: "2001:db8::1", Port: 22},
+			expected: "[2001:db8::1]:22",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, tt.endpoint.Addr())
+		})
+	}
+}
