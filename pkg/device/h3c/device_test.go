1650ce6996cd181ab429b5deb777041c4aa3fdac
Aleksandr Balezin <gescheit@yandex-team.ru>
2025-02-06 08:08:39 +0300

    fix: better h3c support

--- pkg/device/h3c/device_test.go
+++ pkg/device/h3c/device_test.go
@@ -10,6 +10,9 @@ func TestDeviceErrors(t *testing.T) {
 	errorCases := [][]byte{
 		[]byte(" % Error: Unrecognized command found at '^' position."),
 		[]byte("                                   ^\r\n % Error:Too many parameters found at '^' position."),
+		[]byte("\r\n % Unrecognized command found at '^' position."),
+		[]byte("\r\n % Too many parameters found at '^' position."),
+		[]byte("\r\n % Incomplete command found at '^' position."),
 	}
 	testutils.ExprTester(t, errorCases, errorExpression)
 }
@@ -17,6 +20,7 @@ func TestDeviceErrors(t *testing.T) {
 func TestDevicePrompt(t *testing.T) {
 	errorCases := [][]byte{
 		[]byte("\r\n<test>"),
+		[]byte("\r\n(M)<test-test>"), // mmi-mode
 	}
 	testutils.ExprTester(t, errorCases, promptExpression)
 }
