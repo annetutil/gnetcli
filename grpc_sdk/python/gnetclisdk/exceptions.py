27de9d1507a2dc48649daec81d51751891175cd2
Aleksandr Balezin <gescheit@yandex-team.ru>
2025-02-07 12:16:32 +0300

    fix empty reason

--- grpc_sdk/python/gnetclisdk/exceptions.py
+++ grpc_sdk/python/gnetclisdk/exceptions.py
@@ -118,8 +118,11 @@ def parse_grpc_error(grpc_error: grpc.aio.AioRpcError) -> Tuple[Type[GnetcliExce
     detail = ""
     if grpc_error.details():
         detail = grpc_error.details()  # type: ignore
-    reason = error_info.reason
-    metadata = dict(error_info.metadata)
+    reason = ""
+    metadata = None
+    if error_info:
+        reason = error_info.reason
+        metadata = dict(error_info.metadata)
     if code == grpc.StatusCode.UNAVAILABLE and detail == "not ready":
         return NotReady, ""
     if code == grpc.StatusCode.UNAUTHENTICATED:
