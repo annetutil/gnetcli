7781ae01cfd4ea48350f7d15b0e93add3e4bc363
Vladimir Lettiev <thecrux@gmail.com>
2025-10-22 17:46:30 +0300

    Initial support for Eltex
    Patch based on basic gnetcli_conf.yaml tested on a Eltex MES24XX device (SV:10.4.2.1)
    
    ```yaml
    devices:
      - name: eltex
        prompt_expression: '(?P<prompt>\r?[\w\-.:/]+(\(conf(ig)?(-[^)]+)*\))?)(>|#)$'
        password_error_expression: "\nPermission denied, please try again.(\r\n|\n)"
        pager_expression: '\r\x1b\[K\r--More--\x1b\[K'
        features:
          - extra_cr_echo
          - autocmds:
            - set cli pagination off
    ```

---
+++ pkg/device/eltex/eltex.go
@@ -0,0 +1,51 @@
+/*
+Package eltex implements Eltex CLI using genericcli.
+*/
+package eltex
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/annetutil/gnetcli/pkg/cmd"
+	"github.com/annetutil/gnetcli/pkg/device/genericcli"
+	"github.com/annetutil/gnetcli/pkg/expr"
+	"github.com/annetutil/gnetcli/pkg/streamer"
+)
+
+const (
+	loginExpression    = `.*Username:\s?$`
+	questionExpression = `\n(?P<question>.*Continue\? \[Y/N\]:)$`
+	promptExpression   = `(?P<prompt>\r?[\w\-.:/]+(\(conf(ig)?(-[^)]+)*\))?)(>|#)$`
+	errorExpression    = `(` +
+		`\r\n% Invalid input detected at '\^' marker\r\n` +
+		`|\r% Ambiguous Command\r\n` +
+		`|\r% Invalid Command\r\n` +
+		`)`
+	passwordExpression      = `.*password:\s?$`
+	passwordErrorExpression = `\nPermission denied, please try again.(\r\n|\n)`
+	pagerExpression         = `\r\x1b\[K\r--More--\x1b\[K`
+)
+
+var autoCommands = []cmd.Cmd{
+	cmd.NewCmd("set cli pagination off", cmd.WithErrorIgnore()),   //Eltex MES
+}
+
+func NewDevice(connector streamer.Connector, opts ...genericcli.GenericDeviceOption) genericcli.GenericDevice {
+	cli := genericcli.MakeGenericCLI(expr.NewSimpleExprLast200().FromPattern(promptExpression), expr.NewSimpleExprLast200().FromPattern(errorExpression),
+		genericcli.WithLoginExprs(
+			expr.NewSimpleExprLast200().FromPattern(loginExpression),
+			expr.NewSimpleExprLast200().FromPattern(passwordExpression),
+			expr.NewSimpleExprLast200().FromPattern(passwordErrorExpression)),
+		genericcli.WithPager(
+			expr.NewSimpleExprLast200().FromPattern(pagerExpression)),
+		genericcli.WithQuestion(
+			expr.NewSimpleExprLast200().FromPattern(questionExpression)),
+		genericcli.WithAutoCommands(autoCommands),
+		genericcli.WithEchoExprFn(func(c cmd.Cmd) expr.Expr {
+			return expr.NewSimpleExpr().FromPattern(fmt.Sprintf(`%s\r*\n`, regexp.QuoteMeta(string(c.Value()))))
+		}),
+		genericcli.WithTerminalParams(400, 0),
+	)
+	return genericcli.MakeGenericDevice(cli, connector, opts...)
+}
