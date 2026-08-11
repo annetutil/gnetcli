c01a36f2f9e303a113c6e654c69124bd1e8a5daa
Aleksandr Balezin <gescheit@yandex-team.ru>
2026-08-11 12:27:15 +0300

    fix: proper error if remote host in asnwer

--- pkg/streamer/console/console.go
+++ pkg/streamer/console/console.go
@@ -350,9 +350,15 @@ func (m *Streamer) connectConsolePort(ctx context.Context) (err error) {
 	case bytes.HasPrefix(res, []byte("ambiguous console abbreviation")) ||
 		bytes.HasSuffix(res, []byte(" not found\r\n")):
 		return ThrowBadConsolePortException(res)
-	case bytes.HasPrefix(res, []byte("@")) || unicode.IsDigit(rune(res[0])):
-		//redirect
-		newPort, err := strconv.Atoi(strings.TrimSpace(string(res)))
+	case bytes.HasPrefix(res, []byte("#")) || unicode.IsDigit(rune(res[0])):
+		// redirect
+		redirect := strings.TrimSpace(string(res))
+		var newPort int
+		if strings.HasPrefix(redirect, "@") {
+			return fmt.Errorf("conserver redirecting to host %v", redirect)
+		}
+		// just port
+		newPort, err = strconv.Atoi(redirect)
 		if err != nil {
 			return err
 		}
